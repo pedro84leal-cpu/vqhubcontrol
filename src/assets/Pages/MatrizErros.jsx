a442c4cd56f4915b053663ae3f0b48ad2c989921
Pedro Leal <pedro.84leal@gmail.com>
2026-03-17 23:21:03 +0000

    Novas funcionalidades

--- src/assets/Pages/MatrizErros.jsx
+++ src/assets/Pages/MatrizErros.jsx
@@ -1,15 +1,65 @@
-import  '../../assets/Pages/MatrizErros.css'
 import MenuPrincipal from '../Components/Menus/menuPrincipal';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom'; 
+import styles from './Mirtilio_Menu/subMenu.module.css'
+import iconEsquemas from '../Images/Esquemas.png';
+import iconComponents from '../Images/componentes.png';
+import Eflow from '../Images/e-flow.png';
 
 
 
 function MatrizErros() {
 
-  return (
+ return (
+    <div className={styles.MenuContainer}>
+      <MenuPrincipal/>
+      <div className={styles.Menu}>
+        <header className={styles.MenuTitulo}>
+          <h1>
+            Matriz
+          </h1>
+          <p>
+            Erros
+          </p>
 
-  <div>
-    <MenuPrincipal/>
-  </div>
+        </header>
+        <aside>
+          <nav className={styles.MenuItem}>
+
+            <Link to="/mirtilo-console" onClick={(e) => {e.preventDefault();ativarPino(33, "/mirtilo-console");}}>
+              <img src={iconEsquemas} className={styles.MenuIcon} alt="Esquemas"/>
+
+               <div class={styles.textoContainer}>       
+                <span>Esquemas PCB's</span>
+                {/*<small className={styles.MenuSubtitulo}>esquemas pcb's</small>*/}
+              </div>
+
+            </Link>
+
+            <Link to="/satelite" onClick={() => desativarPino(33)} >
+              <img src={iconComponents} className={styles.MenuIcon} alt="Satélites"/>
+
+              <div className={styles.textoContainer}>
+                <span>Esq. Componentes</span>
+                {/*<small className={styles.MenuSubtitulo}>4 - output's externas </small>*/}
+              </div>
+
+            </Link>
+
+           {/*<Link to="/eflow">  
+              <img src={Eflow} className={styles.MenuIcon} alt="e-Flow"/>
+          
+              <div className={styles.textoContainer}>
+                <span>teste e-flow</span>
+              </div>
+                        
+            </Link>*/}
+
+          </nav>
+        </aside>
+      </div>
+    
+    </div>
     
   );
 }
